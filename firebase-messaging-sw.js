3f67eec324b75dea946f29b5683df17fb0ab5e7e
elbieh <sayed.elbieh@yourparts.com>
2022-05-08 14:51:08 +0200

    first commit

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,30 @@
+// Scripts for firebase and firebase messaging
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
+
+// Initialize the Firebase app in the service worker by passing the generated config
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log("Received background message ", payload);
+
+  const notificationTitle = payload.title;
+  const notificationOptions = {
+    body: payload.body,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
